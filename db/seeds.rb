a40694472d0810de4a45afcfccfc8bd37f087b1e
Chandraprakash <cpshivhare786@gmail.com>
2021-12-31 03:18:25 +0530

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  class << self
+    def create_data
+      1..5.times.each do
+        region = create_region_data
+        puts "Region #{region.title} data sucessfully created!"
+      end
+
+      puts "\n ----------------------------"
+      user = FactoryBot.create(:user)
+      puts "User successfully created! Email: #{user.email} Password: password"
+      puts "\n ----------------------------"
+
+      admin = FactoryBot.create(:admin)
+      puts "Admin successfully created! Email: #{admin.email} Password: password"
+      puts "\n"
+      puts "Data has been created! \u{1F64F}"
+    end
+
+    def create_region_data
+      region = FactoryBot.create(:region)
+      FactoryBot.create_list(:product, 3, region: region)
+
+      region
+    end
+  end
+end
+
+if User.exists?
+  puts 'Data is alreay exists!'
+  puts "\n Do you want to remove old data and create new data press yes/no"
+  if $stdin.gets.chomp == 'yes'
+    Rake::Task['db:reset'].invoke
+    Seed.create_data
+  end
+else
+  Seed.create_data
+end
+
